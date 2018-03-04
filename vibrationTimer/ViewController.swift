26b3a0166481741589bc946a4d405476c02bdc09
daihayas <daihayas0915@gmail.com>
2018-03-04 15:49:30 +0900

    Initial Commit

---
+++ vibrationTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vibrationTimer
+//
+//  Created by 林大地 on 2018/03/04.
+//  Copyright © 2018年 林大地. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
